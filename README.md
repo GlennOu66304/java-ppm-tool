28752eecff231c0bacbb1e6c6f0b6af5208da017
Glenn <2738328919@qq.com>
2022-10-12 19:59:30 +0800

    h2

---
+++ README.md
@@ -0,0 +1,7 @@
+## [**I think there is a miss in the instructions to configure application.properties to connect to H2 database**](https://www.udemy.com/course/full-stack-project-spring-boot-20-react-redux/learn/lecture/11795826#questions/11641338)
+
+```
+spring.h2.console.enabled=true
+spring.datasource.url=jdbc:h2:mem:testdb
+```
+
---
+++ src/main/java/io/agileintelligence/ppmtool/domain/Project.java
@@ -0,0 +1,46 @@
+package io.agileintelligence.ppmtool.domain;
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class Project {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    private String projectName;
+    private String projectIdentifier;
+    private String description;
+    private Date start_date;
+    private Date end_date;
+    private Date created_At;
+    private Date updated_At;
+    
+    public Project(){
+
+    }
+    public long getId() {
+        return id;
+        
+    }
+    public void setId(long id) {
+        this.id = id;
+        
+    }
+    public String getProjectName() {
+        return projectName;
+        
+    }
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+        
+    }
+    @PrePersist
+    protected void onCreate() {
+        this.created_At = new Date();
+        
+    }
+    @PreUpdate
+    protected void onUpdate() {
+        this.updated_At = new Date();
+    }
+}
