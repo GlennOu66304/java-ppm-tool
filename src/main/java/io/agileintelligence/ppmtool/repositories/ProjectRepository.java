19023a35eba6dd78a4cb0fc71e217148c15c3f4c
Glenn <2738328919@qq.com>
2022-10-17 17:22:46 +0800

    bug fix

--- src/main/java/io/agileintelligence/ppmtool/repositories/ProjectRepository.java
+++ src/main/java/io/agileintelligence/ppmtool/repositories/ProjectRepository.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProjectRepository extends CrudRepository<Project, Long> {
+ Project findByProjectIdentifier(String projectIdentifier);
     @Override
     Iterable<Project> findAllById(Iterable<Long> iterable);
 
--- src/main/java/io/agileintelligence/ppmtool/services/ProjectService.java
+++ src/main/java/io/agileintelligence/ppmtool/services/ProjectService.java
@@ -4,23 +4,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import io.agileintelligence.ppmtool.domain.Project;
-import io.agileintelligence.ppmtool.exception.ProjectException;
+// import io.agileintelligence.ppmtool.exception.ProjectException;
 import io.agileintelligence.ppmtool.repositories.ProjectRepository;
 
 @Service
 public class ProjectService {
     @Autowired
-    private ProjectRepository projectRepository;
+    private static ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project) {
-        try {
-            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
-            return projectRepository.save(project);
-        } catch (Exception e) {
+        // try {
+        //     project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+        //     return projectRepository.save(project);
+        // } catch (Exception e) {
 
-            throw new ProjectException(
-                    "Project Id '" + project.getProjectIdentifier().toUpperCase() + "  Already exist '");
-        }
+        //     throw new ProjectException(
+        //             "Project Id '" + project.getProjectIdentifier().toUpperCase() + "'  Already exist ");
+        // }
+        
+        return projectRepository.save(project);
+    }
 
+    public static Project findProjectByProjectIdentifier(String projectId) {
+        return projectRepository.findByProjectIdentifier(projectId);
     }
 }
